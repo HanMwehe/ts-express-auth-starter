41f9cf390877985414762bdc5330aeeea36a2299
Hanmwehe <han678556@gmail.com>
2025-04-25 16:58:34 +0700

    Express-ts-starter

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,4 @@ const app = express();
 
 const PORT = 5000
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
